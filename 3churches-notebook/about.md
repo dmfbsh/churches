462ecb56b4a54f11a0be2e0ecebcce85ad79289e
dmfbsh <51413860+dmfbsh@users.noreply.github.com>
2023-01-13 17:57:30 +0000

    Updates

---
+++ 3churches-notebook/about.md
@@ -0,0 +1,8 @@
+---
+layout: page
+title: About
+---
+
+<p class="message">
+This is the About page.
+</p>
---
+++ _build.bat
@@ -0,0 +1,7 @@
+@ECHO OFF
+
+CD C:\Users\David\Documents\OneDrive\Documents\My Documents\GitHub\churches
+
+jekyll build --verbose --config _config.yml
+
+PAUSE
