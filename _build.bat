dd199df6d009a248d6e08da58f2981744a00fc13
dmfbsh <51413860+dmfbsh@users.noreply.github.com>
2023-01-31 17:02:05 +0000

    Updates

--- _build.bat
+++ _build.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 
-CD C:\Users\David\Documents\OneDrive\Documents\My Documents\GitHub\churches
+CD C:\Users\David\Documents\Dropbox\Notebooks\Notebook\3. Churches\GitHub
 
 jekyll build --verbose --config _config.yml
 
